6285bc50eccef6649ecfb80c035df19e16e1f81f
alensaito1 <erwinsmithscout1@gmail.com>
2021-10-12 14:03:32 +0530

    add Custom Emitter

---
+++ src/Emitter.ts
@@ -0,0 +1,6 @@
+import { EventEmitter } from "events";
+import { AureolinEventEmitter } from "./types";
+
+const Emitter = EventEmitter as unknown as new () => AureolinEventEmitter
+
+export default Emitter
