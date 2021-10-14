6586c4e0c015779e954292d56c8501c248d09d1d
alensaito1 <erwinsmithscout1@gmail.com>
2021-10-14 13:37:08 +0530

    chore(source): update `getParam()` to get request body

--- src/handlers/param.ts
+++ src/handlers/param.ts
@@ -6,6 +6,8 @@ export const getParam = (ctx: Context, name: string, meta: Record<string, string
             return meta?.['query'] ? ctx.query?.[meta['query']] : ctx.query
         case 'param':
             return meta?.['param'] ? ctx.params?.[meta['param']] : ctx.params
+        case 'body':
+            return ctx.request.body
         case 'ctx':
             return ctx
         default:
