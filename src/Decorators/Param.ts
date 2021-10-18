84738995c4769a1f44170f424a779ca20096c870
alensaito1 <erwinsmithscout1@gmail.com>
2021-10-18 12:51:20 +0530

    feat(decorators): add new parameter decorators

--- src/Decorators/Param.ts
+++ src/Decorators/Param.ts
@@ -64,3 +64,50 @@ export const Body = (): ParameterDecorator => {
         })
     }
 }
+
+/**
+ * Marks a parameter to be injected with request
+ */
+export const Req = (): ParameterDecorator => {
+    return (controller: any, key: string | symbol, index: number): void => {
+        paramStore.register({
+            controller: controller.constructor.name,
+            propertyKey: key.toString(),
+            index,
+            type: 'req'
+        })
+    }
+}
+
+/**
+ * Marks a parameter to be injected with response
+ */
+export const Res = (): ParameterDecorator => {
+    return (controller: any, key: string | symbol, index: number): void => {
+        paramStore.register({
+            controller: controller.constructor.name,
+            propertyKey: key.toString(),
+            index,
+            type: 'res'
+        })
+    }
+}
+
+/**
+ * Marks a parameter to be injected with request header
+ * If no header name is specified, then the whole header object will be injected.
+ * @param header - The header name.
+ */
+export const Header = (header?: string): ParameterDecorator => {
+    return (controller: any, key: string | symbol, index: number): void => {
+        paramStore.register({
+            controller: controller.constructor.name,
+            propertyKey: key.toString(),
+            index,
+            type: 'header',
+            meta: {
+                header
+            }
+        })
+    }
+}
