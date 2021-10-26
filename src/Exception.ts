727136ca2279eb2798731602efd3af6bec4ef8e0
alensaito1 <erwinsmithscout1@gmail.com>
2021-10-26 18:17:45 +0530

    feat(): add default mesages for all exported Http Exceptions

--- src/Exception.ts
+++ src/Exception.ts
@@ -10,36 +10,50 @@ export class Exception extends Error {
     }
 }
 
-const generateHttpExceptionClass = (code: HttpStatusCode) => {
+const generateHttpExceptionClass = (code: HttpStatusCode, _data?: any) => {
     const ExceptionClass = class extends Exception {
         constructor(data: any) {
-            super(data, code)
+            super(data || _data, code)
         }
     }
-
     return ExceptionClass
 }
 
-export const BadRequestException = generateHttpExceptionClass(HttpStatus.BAD_REQUEST)
-export const UnauthorizedException = generateHttpExceptionClass(HttpStatus.UNAUTHORIZED)
-export const ForbiddenException = generateHttpExceptionClass(HttpStatus.FORBIDDEN)
-export const NotFoundException = generateHttpExceptionClass(HttpStatus.NOT_FOUND)
-export const MethodNotAllowedException = generateHttpExceptionClass(HttpStatus.METHOD_NOT_ALLOWED)
-export const NotAcceptableException = generateHttpExceptionClass(HttpStatus.NOT_ACCEPTABLE)
-export const ConflictException = generateHttpExceptionClass(HttpStatus.CONFLICT)
-export const GoneException = generateHttpExceptionClass(HttpStatus.GONE)
-export const PreconditionFailedException = generateHttpExceptionClass(HttpStatus.PRECONDITION_FAILED)
-export const UnsupportedMediaTypeException = generateHttpExceptionClass(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-export const InternalServerException = generateHttpExceptionClass(HttpStatus.INTERNAL_SERVER_ERROR)
-export const NotImplementedException = generateHttpExceptionClass(HttpStatus.NOT_IMPLEMENTED)
-export const BadGatewayException = generateHttpExceptionClass(HttpStatus.BAD_GATEWAY)
-export const ServiceUnavailableException = generateHttpExceptionClass(HttpStatus.SERVICE_UNAVAILABLE)
-export const GatewayTimeoutException = generateHttpExceptionClass(HttpStatus.GATEWAY_TIMEOUT)
-export const HttpVersionNotSupportedException = generateHttpExceptionClass(HttpStatus.HTTP_VERSION_NOT_SUPPORTED)
-export const VariantAlsoNegotiatesException = generateHttpExceptionClass(HttpStatus.VARIANT_ALSO_NEGOTIATES)
-export const InsufficientStorageException = generateHttpExceptionClass(HttpStatus.INSUFFICIENT_STORAGE)
-export const BandwidthLimitExceededException = generateHttpExceptionClass(HttpStatus.BANDWIDTH_LIMIT_EXCEEDED)
-export const NotExtendedException = generateHttpExceptionClass(HttpStatus.NOT_EXTENDED)
+export const BadRequestException = generateHttpExceptionClass(HttpStatus.BAD_REQUEST, 'Bad Request')
+export const UnauthorizedException = generateHttpExceptionClass(HttpStatus.UNAUTHORIZED, 'Unauthorized')
+export const ForbiddenException = generateHttpExceptionClass(HttpStatus.FORBIDDEN, 'Forbidden')
+export const NotFoundException = generateHttpExceptionClass(HttpStatus.NOT_FOUND, 'Not Found')
+export const MethodNotAllowedException = generateHttpExceptionClass(HttpStatus.METHOD_NOT_ALLOWED, 'Method Not Allowed')
+export const NotAcceptableException = generateHttpExceptionClass(HttpStatus.NOT_ACCEPTABLE, 'Not Acceptable')
+export const ConflictException = generateHttpExceptionClass(HttpStatus.CONFLICT, 'Conflict')
+export const UnprocessableEntityException = generateHttpExceptionClass(
+    HttpStatus.UNPROCESSABLE_ENTITY,
+    'Unprocessable Entity'
+)
+export const InternalServerErrorException = generateHttpExceptionClass(
+    HttpStatus.INTERNAL_SERVER_ERROR,
+    'Internal Server Error'
+)
+export const ServiceUnavailableException = generateHttpExceptionClass(
+    HttpStatus.SERVICE_UNAVAILABLE,
+    'Service Unavailable'
+)
+export const GatewayTimeoutException = generateHttpExceptionClass(HttpStatus.GATEWAY_TIMEOUT, 'Gateway Timeout')
+export const HttpVersionNotSupportedException = generateHttpExceptionClass(
+    HttpStatus.HTTP_VERSION_NOT_SUPPORTED,
+    'HTTP Version Not Supported'
+)
+export const VariantAlsoNegotiatesException = generateHttpExceptionClass(
+    HttpStatus.VARIANT_ALSO_NEGOTIATES,
+    'Variant Also Negotiates'
+)
+export const InsufficientStorageException = generateHttpExceptionClass(
+    HttpStatus.INSUFFICIENT_STORAGE,
+    'Insufficient Storage'
+)
+export const LoopDetectedException = generateHttpExceptionClass(HttpStatus.LOOP_DETECTED, 'Loop Detected')
+export const NotExtendedException = generateHttpExceptionClass(HttpStatus.NOT_EXTENDED, 'Not Extended')
 export const NetworkAuthenticationRequiredException = generateHttpExceptionClass(
-    HttpStatus.NETWORK_AUTHENTICATION_REQUIRED
+    HttpStatus.NETWORK_AUTHENTICATION_REQUIRED,
+    'Network Authentication Required'
 )
