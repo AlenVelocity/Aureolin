23b7d6af088bfcee403d6afd3591e8fe2d77fa2d
alensaito1 <erwinsmithscout1@gmail.com>
2021-11-02 11:20:12 +0530

    feat(): `ComponentNode` Support

--- src/handlers/route.ts
+++ src/handlers/route.ts
@@ -1,4 +1,4 @@
-import { ElementNode } from 'async-jsx-html'
+import { ComponentNode, ElementNode } from 'async-jsx-html'
 import { Middleware } from 'koa'
 import { Exception } from '..'
 import paramStore from '../store/param'
@@ -20,7 +20,7 @@ export const handleRoute = (
             const args = sortedParams.map((param) => getParam(context, param.type, param.meta))
             let res = await cb(...args)
             if (res) {
-                if (res instanceof ElementNode) {
+                if (res instanceof ElementNode || res instanceof ComponentNode) {
                     res = await res.render()
                 }
                 context.body = res
