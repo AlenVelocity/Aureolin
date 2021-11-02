b138b701027d1f676d69c47733b92493a5f6d5b6
alensaito1 <erwinsmithscout1@gmail.com>
2021-11-02 17:24:37 +0530

    feat(): implement config loader #31

---
+++ src/config.ts
@@ -0,0 +1,35 @@
+import { existsSync } from 'fs'
+import { join } from 'path'
+
+/**
+ * Configuration object type for Aureolin
+ */
+export type Config = {
+    /**
+     * Port to listen on
+     * @type {number}
+     * @default 3000
+     */
+    port?: number
+    /**
+     * Dir where `contollers` and `providers` are located
+     * @type {string}
+     * @default process.cwd() + '/src'
+     */
+    root: string
+}
+
+/**
+ * Loads config from `aureolin.config.js`
+ * @returns {Config} Loaded config object
+ */
+export const loadConfig = (): Config => {
+    const path = process.cwd().concat('aureolin.config.js')
+    if (!existsSync(path)) throw new Error('Config file not found')
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const config = require(path) as Partial<Config>
+    return {
+        port: config.port ?? 3000,
+        root: config.root ?? join(process.cwd(), 'src')
+    }
+}
