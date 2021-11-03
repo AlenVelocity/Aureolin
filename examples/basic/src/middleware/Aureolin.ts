dab3cdf08b517322f6a63b52185f14289f569701
alensaito1 <erwinsmithscout1@gmail.com>
2021-11-03 09:42:33 +0530

    style(): run prettier

--- examples/basic/src/middleware/Aureolin.ts
+++ examples/basic/src/middleware/Aureolin.ts
@@ -4,7 +4,7 @@ export const Aureolin = () => {
     return async (ctx: Context, next: NextFunction) => {
         ctx.res.setHeader('X-Aureolin', 'Aureolin')
         // eslint-disable-next-line @typescript-eslint/no-var-requires
-        ctx.res.setHeader('X-Aureolin-Version', (require('../../package.json')).version)
+        ctx.res.setHeader('X-Aureolin-Version', require('../../package.json').version)
         await next()
     }
 }
