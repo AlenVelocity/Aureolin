c336f6a285d71136bf6b5dd151ee90dd7f9af232
alensaito1 <erwinsmithscout1@gmail.com>
2021-11-03 09:43:14 +0530

    feat(): add nano-jsx exports #33

--- src/handlers/route.ts
+++ src/handlers/route.ts
@@ -1,4 +1,3 @@
-import { ComponentNode, ElementNode } from 'async-jsx-html'
 import { Middleware } from 'koa'
 import { Exception } from '..'
 import paramStore from '../store/param'
@@ -18,11 +17,8 @@ export const handleRoute = (
     return async (context: Context): Promise<void> => {
         try {
             const args = sortedParams.map((param) => getParam(context, param.type, param.meta))
-            let res = await cb(...args)
+            const res = await cb(...args)
             if (res) {
-                if (res instanceof ElementNode || res instanceof ComponentNode) {
-                    res = await res.render()
-                }
                 context.body = res
             }
         } catch (err) {
