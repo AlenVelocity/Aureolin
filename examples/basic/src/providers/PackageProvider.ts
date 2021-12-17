c38b5eb01f4fa2c8a3e081c4496f604b89cdec99
alensaito1 <erwinsmithscout1@gmail.com>
2021-12-17 22:02:56 +0530

    style(example): remove empty constructors

--- examples/basic/src/providers/PackageProvider.ts
+++ examples/basic/src/providers/PackageProvider.ts
@@ -2,7 +2,6 @@ import { Provider } from 'aureolin'
 
 @Provider('package')
 export default class PackageProvider {
-    constructor() {}
 
     public get = async () => {
         return require('../../package.json')
--- examples/basic/src/providers/TimeProvider.ts
+++ examples/basic/src/providers/TimeProvider.ts
@@ -2,7 +2,6 @@ import { Provider } from 'aureolin'
 
 @Provider('time')
 export default class TimeProvider {
-    constructor() {}
 
     public get = (): string => {
         return new Date().toLocaleTimeString()
