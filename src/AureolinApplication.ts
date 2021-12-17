d23dff017a400254a86519a18d2cdbf123f78713
alensaito1 <erwinsmithscout1@gmail.com>
2021-12-17 15:28:00 +0530

    fix(): public serve

--- src/AureolinApplication.ts
+++ src/AureolinApplication.ts
@@ -109,8 +109,8 @@ export class AureolinApplication extends Emitter {
         })
 
         if (this.options.public) {
-            const { path = '/public', dir = './public', ...options } = this.options.public
-            this.router.use(path, serve(dir, options))
+            const { dir = './public', ...options } = this.options.public
+            this.router.use(serve(dir, options))
         }
 
         for (const [controller, { path }] of endpointStore.getControllers()) {
