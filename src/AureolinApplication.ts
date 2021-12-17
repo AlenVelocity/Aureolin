b36644187aaa5d4137070e80589da582bc961ded
alensaito1 <erwinsmithscout1@gmail.com>
2021-12-17 17:40:37 +0530

    fix(): public serve

--- src/AureolinApplication.ts
+++ src/AureolinApplication.ts
@@ -112,7 +112,7 @@ export class AureolinApplication extends Emitter {
 
         if (this.options.public) {
             const { path = '/public', dir = './public', ...options } = this.options.public
-            this.router.use(mount(path, serve(dir, options)))
+            this.server.use(mount(path, serve(dir, options)))
         }
 
         if (this.options.views) {
