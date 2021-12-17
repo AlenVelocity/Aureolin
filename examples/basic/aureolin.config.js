c120752fd07dad12241838c503965a0ccaeab9fe
alensaito1 <erwinsmithscout1@gmail.com>
2021-12-17 16:27:08 +0530

    deps(): add `koa-views`

--- examples/basic/aureolin.config.js
+++ examples/basic/aureolin.config.js
@@ -3,5 +3,10 @@ const { join } = require('path')
 /** @type {import('aureolin').Config} */
 module.exports = {
     root: join(process.cwd(), process.env.NODE_ENV === 'production' ? 'dist' : 'src'),
-    port: 3000
+    port: 3000,
+
+
+    public: {
+        dir: '.'
+    }
 }
